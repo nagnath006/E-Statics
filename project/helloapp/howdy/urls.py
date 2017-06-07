f94ec3ea6876a706829fd7ade5ab438ee5ae82dc
nagnath006 <nagnathdahale@gmail.com>
2017-06-07 02:48:33 -0700

    UI Changes

--- project/helloapp/howdy/urls.py
+++ project/helloapp/howdy/urls.py
@@ -3,11 +3,11 @@ from howdy import views
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
-    url(r'^$', views.HomePageView.as_view()),
+    url(r'^index/$', views.HomePageView.as_view()),
 	url(r'^aboutus/$',views.AboutPageView.as_view()),
 	url('', include('social.apps.django_app.urls', namespace='social')),
 	url('', include('django.contrib.auth.urls', namespace='auth')),
-	
+
 
 ]
       
